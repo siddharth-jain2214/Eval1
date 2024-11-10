f10aecdeda0413a8b17cb000d94b3e90e63d9aec
ShubhamJindal98 <sjindal345@gmail.com>
2024-11-10 18:26:59 +0530

    header react

--- finalEvaluation/finalEvaluation/components/Navbar/Navbar.jsx
+++ finalEvaluation/finalEvaluation/components/Navbar/Navbar.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
-import HeaderTop from './HeaderTop'
-import HeaderRest from './HeaderRest'
-
-const Navbar = () => {
+import React from 'react';
+import './Header.css'; // Import the CSS for the header
+const Header = () => {
   return (
-    <>
-     <HeaderTop></HeaderTop> 
-     <HeaderRest></HeaderRest>
-    </>
-  )
-}
+    <header className="header" data-header>
+      <div className="overlay" data-overlay></div>
 
-export default Navbar
+      <div className="header-top">
+        <div className="container">
+          <div className="header-top-heading">
+            <ul className="header-top-list">
+              <li>
+                <a href="mailto:info@propertypulse.com" className="header-top-link">
+                  {/* <IonIcon icon={mailOutline} /> */}
+                  <span>info@propertypulse.com</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="header-top-link">
+                  {/* <IonIcon icon={locationOutline} /> */}
+                  <address>15/A NYC, New York, USA</address>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="wrapper">
+            <ul className="header-top-social-list">
+              <li>
+                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="header-top-social-link">
+                  {/* <IonIcon icon={logoFacebook} /> */}
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="header-top-social-link">
+                  {/* <IonIcon icon={logoTwitter} /> */}
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="header-top-social-link">
+                  {/* <IonIcon icon={logoInstagram} /> */}
+                </a>
+              </li>
+              <li>
+                <a href="https://in.pinterest.com/" target="_blank" rel="noopener noreferrer" className="header-top-social-link">
+                  {/* <IonIcon icon={logoPinterest} /> */}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="header-rest">
+        <div className="header_logo">
+          <span><img src="images/service-1.png" alt="logo" width="100px" /></span>
+          <span className="header_logo_cap">PropertyPulse</span>
+        </div>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#service">Service</a></li>
+          <li><a href="#properties1">Property</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <ul>
+          <li><a href="./login.html"><button className="header_btn">LOGIN</button></a></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- finalEvaluation/finalEvaluation/src/App.jsx
+++ finalEvaluation/finalEvaluation/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/Navbar/Navbar'
+import { mailOutline, locationOutline, logoFacebook, logoTwitter, logoInstagram, logoPinterest } from 'ionicons/icons';
+
 
 const App = () => {
   return (
     <div>
-      
+      <Header></Header>
     </div>
   )
 }
